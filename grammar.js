36b8856b692a83090bee671f14d4f59da0f6d7f5
rktdnjs <rktdnjs@naver.com>
2024-02-07 16:16:36 +0900

    [feat] : tree-sitter-smallbasic grammar.js
    - sbparser 69 Lines - CompExpr 심볼까지 rules 정의
    - 문자열을 표현할 때 ''가 아닌 ""가 쓰이도록 수정
    - sbparser 상에서 공백으로 나오는 부분을 일단 다음과 같이 주석 처리 해놓음
    ㄴ// *공백이 오는 경우 표현(확인 후 작성 필요)
    - CR 심볼의 경우 /\r\n|\n/ 표현식으로 정의

--- grammar.js
+++ grammar.js
@@ -1,5 +1,5 @@
 module.exports = grammar({
-    name: 'nodejs',
+    name: "nodejs",
   
     rules: {
       // TODO: add the actual grammar rules
@@ -11,23 +11,100 @@ module.exports = grammar({
   
       // junk: $ => /[^%@\s\n\t][^%@]*/,
   
-      // comment: $ => token(seq('%', /.*/)),
-  
+      // comment: $ => token(seq("%", /.*/)),
+
       // _command_or_entry: $ => seq("@", /[a-zA-Z]+/)
 
+
+      // Non-Terminals
       Start: $ => $.Prog,
 
       Prog: $ => $.MoreThanOneStmt,
 
       Stmt: $ => choice(
         $.ExprStatement,
-        seq( /[Ww][Hh][Ii][Ll][Ee]/,  $.Expr, $.CRStmtCRs, /[Ee][Nn][Dd][Ww][Hh][Ii][Ll][Ee]/  ),
-        seq($.ID, ':'),
+        seq( /[Ww][Hh][Ii][Ll][Ee]/,  $.Expr, $.CRStmtCRs, /[Ee][Nn][Dd][Ww][Hh][Ii][Ll][Ee]/),
+        seq($.ID, ":"),
         seq(/[Gg][Oo][Tt][Oo]/, $.ID),
-        seq(/[Ff][Oo][Rr]/, $.ID, '=', $.Expr, /[Tt][Oo]/, $.Expr, $.OptStep, $.CRStmtCRs, /[Ee][Nn][Dd][Ff][Oo][Rr]/),
+        seq(/[Ff][Oo][Rr]/, $.ID, "=", $.Expr, /[Tt][Oo]/, $.Expr, $.OptStep, $.CRStmtCRs, /[Ee][Nn][Dd][Ff][Oo][Rr]/),
         seq(/[Ss][Uu][Bb]/, $.ID, $.CRStmtCRs, /[Ee][Nn][Dd][Ss][Uu][Bb]/),
         seq(/[Ii][Ff]/, $.Expr, /[Tt][Hh][Ee][Nn]/, $.CRStmtCRs, $.MoreThanZeroElseIf),
-        seq()
+        // *공백이 오는 경우 표현(확인 후 작성 필요)
+      ),
+
+      MoreThanZeroElseIf: $ => choice(
+        $.OptionalElse,
+        seq(/[Ee][Ll][Ss][Ee][Ii][Ff]/, $.Expr, /[Tt][Hh][Ee][Nn]/, $.CRStmtCRs, $.MoreThanZeroElseIf),
+      ),
+      
+      OptionalElse: $ => choice(
+        /[Ee][Nn][Dd][Ii][Ff]/,
+        seq(/[Ee][Ll][Ss][Ee]/, $.CRStmtCRs, /[Ee][Nn][Dd][Ii][Ff]/)
+      ),
+
+      ExprStatement: $ => choice(
+        seq($.ID, "=", $.Expr),
+        seq($.ID, ".", $.ID, "=", $.Expr),
+        seq($.ID, ".", $.ID, "(", $.Exprs, ")"),
+        seq($.ID, "(", ")"),
+        seq($.ID, $.Idxs, "=", $.Expr),
+      ),
+      
+      // CRStmtCRS -> CR TheRest
+      // CR은 다음 2가지 "\r\n" or "\n"
+      // "\r\n" or "\n" 정규표현식 : /\r\n|\n/ 캐리지리턴이 있거나 없거나 둘 중 하나
+      CRStmtCRs: $ => seq(choice(/\r\n|\n/) ,$.TheRest),
+
+      TheRest: $ => choice(
+        // *공백이 오는 경우 표현(확인 후 작성 필요)
+        seq($.Stmt, choice(/\r\n|\n/), $.TheRest),
+      ),
+
+      MoreThanOneStmt: $ => choice(
+        $.Stmt,
+        seq($.Stmt, choice(/\r\n|\n/), $.MoreThanOneStmt),
+      ),
+
+      OptStep: $ => choice(
+        // *공백이 오는 경우 표현(확인 후 작성 필요),
+        seq(/[Ss][Tt][Ee][Pp]/, $.Expr),
+      ),
+
+      Expr: $ => $.CondExpr,
+
+      Exprs: $ => choice(
+        // *공백이 오는 경우 표현(확인 후 작성 필요),
+        $.MoreThanOneExpr,
+      ),
+
+      MoreThanOneExpr: $ => choice(
+        $.Expr,
+        seq($.Expr, $.MoreThanOneExpr),
+      ),
+
+      CondExpr: $ => $.OrExpr,
+
+      OrExpr: $ => choice(
+        seq($.OrExpr, /[Oo][Rr]/, $.AndExpr),
+      ),
+
+      AndExpr: $ => choice(
+        seq($.AndExpr, /[Aa][Nn][Dd]/, $.EqNeqExpr),
+      ),
+      
+      // <>의 뜻은 NotEqual(!=와 같음)
+      EqNeqExpr: $ => choice(
+        seq($.EqNeqExpr, "=", $.CompExpr),
+        seq($.EqNeqExpr, "<>", $.CompExpr),
+        $.CompExpr,
+      ),
+
+      CompExpr: $ => choice(
+        seq($.CompExpr, "<", $.AdditiveExpr),
+        seq($.CompExpr, "<=", $.AdditiveExpr),
+        seq($.CompExpr, ">", $.AdditiveExpr),
+        seq($.CompExpr, ">=", $.AdditiveExpr),
+        $.AdditiveExpr,
       ),
 
       // Terminals
